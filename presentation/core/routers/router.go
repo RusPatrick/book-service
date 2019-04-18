400323a9ed8fb61b6d7d5ecb1337e1dbddea36dd
Rodion <farrahov95@mail.ru>
2019-04-18 16:29:25 +0300

    явная передача БД в authmiddleware

--- presentation/core/routers/router.go
+++ presentation/core/routers/router.go
@@ -3,6 +3,7 @@ package routers
 import (
 	"net/http"
 
+	"github.com/ruspatrick/book-service/application/services"
 	"github.com/ruspatrick/book-service/presentation/controllers"
 	"github.com/ruspatrick/book-service/presentation/middlewares"
 )
@@ -14,10 +15,10 @@ const (
 func NewRouter() *http.ServeMux {
 	router := http.NewServeMux()
 
-	router.Handle(apiV1Url+"/books", middlewares.AuthMiddleware(http.HandlerFunc(controllers.BooksController)))
-	router.Handle(apiV1Url+"/books/", middlewares.AuthMiddleware(http.HandlerFunc(controllers.BookController)))
+	router.Handle(apiV1Url+"/books", middlewares.AuthMiddleware(http.HandlerFunc(controllers.BooksController), services.GetDB()))
+	router.Handle(apiV1Url+"/books/", middlewares.AuthMiddleware(http.HandlerFunc(controllers.BookController), services.GetDB()))
 	router.HandleFunc(apiV1Url+"/signup", controllers.Signup)
 	router.HandleFunc(apiV1Url+"/login", controllers.Login)
-	router.Handle(apiV1Url+"/me", middlewares.AuthMiddleware(http.HandlerFunc(controllers.Me)))
+	router.Handle(apiV1Url+"/me", middlewares.AuthMiddleware(http.HandlerFunc(controllers.Me), services.GetDB()))
 	return router
 }
--- presentation/middlewares/authMiddleware.go
+++ presentation/middlewares/authMiddleware.go
@@ -1,15 +1,15 @@
 package middlewares
 
 import (
+	"database/sql"
 	"log"
 	"net/http"
 	"time"
 
-	"github.com/ruspatrick/book-service/application/services"
 	"github.com/ruspatrick/book-service/domain/models"
 )
 
-func AuthMiddleware(next http.Handler) http.Handler {
+func AuthMiddleware(next http.Handler, db *sql.DB) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
 		cookie, err := req.Cookie("session_id")
 		if err != nil {
@@ -18,7 +18,6 @@ func AuthMiddleware(next http.Handler) http.Handler {
 		}
 
 		query := `SELECT session_id, exp FROM books.sessions WHERE session_id=$1`
-		db := services.GetDB()
 		row := db.QueryRow(query, cookie.Value)
 		var session models.Session
 		if err := row.Scan(&session.ID, &session.Exp); err != nil {
